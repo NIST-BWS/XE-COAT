23a9c32a7bc4faa53b2fd67626431c7c90f59a6b
NIST IXE <rossm@nist.gov>
2014-02-24 16:46:27 -0500

    Update for 1.0.279.29

--- serviceTests/src/test/resources/walkthrough/go.bat
+++
@@ -1,7 +0,0 @@
-set COAT=http://zox:7774
-call curl -X POST %COAT%/FizzBuzz
-call curl -X POST -H "Content-Type:text/xml"  -d @default.xml %COAT%/FizzBuzz/config/default.xml
-call curl -X POST -H "Content-Type:text/xml"  -d @main.xsd    %COAT%/FizzBuzz/schema/main.xsd
-call curl -X POST -H "Content-Type:text/plain" -d @template.vm %COAT%/FizzBuzz/template
-
-call curl %COAT%/FizzBuzz/process
